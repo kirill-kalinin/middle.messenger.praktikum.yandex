282c0151de8b40b2542edf9f5a4759edd1ead24a
Kirill Kalinin <kir1ll@yandex.ru>
2021-02-10 01:51:02 +0300

    PostCSS configured, styles refactored

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,8 @@
 module.exports = (ctx) => ({
   map: ctx.options.map,
   plugins: [
+      require('postcss-import'),
+      require('postcss-nested'),
       require('autoprefixer'),
       require('precss'),
       require('cssnano')({
